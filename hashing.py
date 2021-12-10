2f90adcad557a1b37833f5c1d25418ad4b9fb93f
Levi Graham <70168588+lgraham002@users.noreply.github.com>
2021-12-10 19:53:12 +0000

    Added UI and HashedItem

--- hashing.py
+++ hashing.py
@@ -6,15 +6,64 @@
 #          upgrade final project.
 # Modification Log:
 #   - 2021.12.09 - LG - Added header and started design.
+#   - 2021.12.10 - LG - Added the HashedItem class and documentation
+#                       for the class. Also added constants.
+#   - 2021.12.10 - SR - Started user interface. 
 
 
-def test_input():
-  file = open("short_input.csv", "r", encoding='utf-8-sig')
-  for line in file:
-    print(line)
+# Constants
+MOD = 11 # Mod factor for the hashing algorithm.
+INPUT_FILE = 'short_input.csv' # Input file for tweets.
 
-  file.close()
+# HashedItem - A class to hold the text of a tweet, a hashtag, and
+#              the pointer to the next hashedItem in the stack.
+# 
+# Properties:
+#   tweet - String for the tweet that is held by an object of this
+#           class. 
+#   hashtag - Holds the string for the hashtag that is held by an
+#             object of this class. 
+#   next_pointer - Pointer to the next object in the hashed list.
+class HashedItem:
+  
+  # __init__() - Initializer for the object of this class.
+  #
+  #   Parameters:
+  #     tweet - String for the text of the tweet.
+  #     hashtag - String for the hashtag for this tweet.
+  def __init__(self, tweet, hashtag):
+    self.tweet = tweet
+    self.hashtag = hashtag
+    self.next_pointer = None
 
+# TODO: Add in read_file() method. 
+
+
+
+
+
+
+
+
+
+# user_interface() - This is what the user will interact with when 
+#                    using this application.
+def user_interface():
+  print("\nWelcome to Open Hashed Twitter\n\nFunctions availble: ")
+  print("   Add new tweet (A)\n   Search for hashtag (S)\n")
+
+  user_input = input("What would you like to do? (A/S) ").upper()
+
+  if user_input == "A":
+    pass
+  elif user_input == "S":
+    pass
+  else:
+    restart = input("\nIncorrect input.\nWould you like to restart? (Y/N) ")
+    if restart.upper() == "Y":
+      user_interface()
+    else:
+      print("\nEnding Execution")
 
 if __name__ == "__main__":
-  test_input()
+  user_interface()
---
+++ rileysattempt.py
@@ -0,0 +1,94 @@
+mod = 11
+
+class Hashtag():
+    def __init__(self, hashtag, tweet):
+        self.hashtag = hashtag
+        self.tweet = tweet
+        self.number = 0
+
+def readData():
+    dataFile = open('hashtagList.csv', 'r')
+    objectList=[]
+    for line in dataFile:
+        thisline = line.split(",")
+        hashtag = thisline[0]
+        tweet = thisline[1]
+        objectList.append(Hashtag(hashtag, tweet))
+    dataFile.close()
+    return objectList
+
+
+def hashascii():
+    objectList = readData()
+    for x in objectList:
+        number = 0
+        for charecter in x.hashtag:
+            number = number + ord(charecter)
+        number = number%mod
+        x.number = number
+    return objectList
+
+def listOfLists():
+    objectList = hashascii()
+    twitterList = []
+    for i in range(mod):
+         val =  i
+         i = []
+         for x in objectList:
+             if x.number == val:
+                #print(x.hashtag)
+                i.append(x)
+         twitterList.append(i)
+    #print(len(twitterList))
+    return twitterList
+        
+
+
+
+
+
+def findTweet(hashtag):
+    twitterList = listOfLists()
+    holdingList=[]
+    number = 0
+    for charecter in hashtag:
+        number = number + ord(charecter)
+    number = number%mod
+    for x in twitterList[number]:
+        if x.hashtag == hashtag:
+            holdingList.append(x)
+    return holdingList
+    
+        
+def writeTweet(newhashtag, newtweet):
+    new = Hashtag(newhashtag, newtweet)
+    objectList = listOfLists()
+    number = 0
+    for charecter in new.hashtag:
+        number =number + ord(charecter)
+    
+
+
+    
+def test():
+    twitterList=listOfLists()
+    val=0
+    for x in twitterList:
+        print(val)
+        val=val+1
+        for item in x:
+            print(item.hashtag,' ', item.tweet)
+    
+
+
+def test2():
+    holdingList=findTweet("blessed")
+    for item in holdingList:
+        print(item.tweet)
+
+
+def test3():
+    writeTweet("blessed","There is so much good in the world")
+    
+
+test3()
